dd41382a9cbe804c32847a27f3cd86f38c4f9a01
杨明明 <yangmingming@datatang.com>
2019-06-19 11:24:48 +0800

    '20190619'

--- pro_code.py
+++ pro_code.py
@@ -50,7 +50,34 @@ def longst_plalindrome(parent_string):
     return longst_plalindrome
 
 
+def convert(s, n):
+    """
+        字符串Z字型排列，用列表嵌套列表的形式表示二维表格，以表格填充的形式组合出相应的字符串, numpy中有narray pandas
+        中有DataFrame,都可以表示这样的形式
+    :param s: 字符串
+    :param n: Z 的高度
+    :return:
+    """
+
+    table = [[" " for i in range(len(s))] for i in range(n)]  # 已知次数的循环 用for 比较合适，这是最近才理解的，以前只知道用
+    row = 0
+    for i, item in enumerate(s):
+        if i < n:
+            col = 0
+        else:
+            col = i - n + 1
+        table[row][col] = item
+
+        sing = (i // (n-1)) % 2
+        if not sing:
+            row += 1
+        else:
+            row -= 1
+
+    return ''.join([''.join(item) for item in table]).replace(' ', '')
+
+
 if __name__ == '__main__':
-    s = "dfgsabccbadfgs"
-    result = longst_plalindrome(s)
+    s = "PAYPALISHIRING"
+    result = convert(s, 3)
     print(result)
