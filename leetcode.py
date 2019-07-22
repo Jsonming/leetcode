6938e7f21cc721a7d2d9ae404ca4e7a495e120e4
jsonming <ymmbjcz@126.com>
2019-07-22 13:52:17 +0800

    '20190722'

--- leetcode.py
+++ leetcode.py
@@ -8,27 +8,20 @@
 from itertools import product
 
 
-class Solution:
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
+
+class Solution:
     def letter_combinations(self, digits: str) -> list[str]:
         """
             第17题，根据手机九键的字幕排序，根据给定数字串生产字母可能的组合
         :param digits: 给出的数字串
         :return:
         """
-        # digits_letter_maping = {
-        #     '2': ['a', 'b', 'c'],
-        #     '3': ['d', 'e', 'f'],
-        #     '4': ['g', 'h', 'i'],
-        #     '5': ['j', 'k', 'l'],
-        #     '6': ['m', 'n', 'o'],
-        #     '7': ['p', 'q', 'r', 's'],
-        #     '8': ['t', 'u', 'v'],
-        #     '9': ['w', 'x', 'y', 'z'],
-        # }
-        # result = []
         table = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
-
         if not len(digits):
             return []
 
@@ -40,7 +33,36 @@ class Solution:
                 result = [old + new for old, new in product(result, table[x])]
         return result
 
+    def four_sum(self, nums: list[int], target: int) -> list[list[int]]:
+        """
+            四数之和
+        :param nums: 数字列表
+        :param target: 目标值
+        :return:
+        """
+
+    def remove_element_from_end(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        """
+        dummy = ListNode(-1)
+        dummy.next = head
+        fast = slow = dummy
+
+        while n and fast:
+            fast = fast.next
+            n -= 1
+
+        while fast.next and slow.next:
+            fast = fast.next
+            slow = slow.next
+
+        slow.next = slow.next.next
+        return dummy.next
+
 
 if __name__ == '__main__':
     result = Solution()
-    print(result.letter_combinations('239'))
+    print(result.remove_element_from_end([1, 0, -1, 0, -2, 2]))
