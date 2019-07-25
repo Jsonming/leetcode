0fdc558321cbd1f4137648e3778ec9238dafb677
jsonming <ymmbjcz@126.com>
2019-07-25 14:43:10 +0800

    '20190725'

--- leetcode.py
+++ leetcode.py
@@ -86,7 +86,20 @@ class Solution:
         else:
             return True
 
+    def generate_parenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        res = set(['()'])
+        for i in range(n - 1):
+            tmp = set()
+            for r in res:
+                tmp.update(set([r[:j] + '()' + r[j:] for j in range(len(r))]))
+            res = tmp
+        return list(res)
+
 
 if __name__ == '__main__':
     result = Solution()
-    print(result.is_valid("()"))
+    print(result.generate_parenthesis(3))
